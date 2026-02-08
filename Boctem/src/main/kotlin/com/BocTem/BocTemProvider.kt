138cd67aad4650abc98f6a1c50b4f27386d63d9b
dah215 <duongah514hp@gmail.com>
2026-02-08 19:18:42 +0700

    Update BocTemProvider.kt

--- Boctem/src/main/kotlin/com/BocTem/BocTemProvider.kt
+++ Boctem/src/main/kotlin/com/BocTem/BocTemProvider.kt
@@ -2,6 +2,7 @@ package com.BocTem
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.MainAPI
 
 @CloudstreamPlugin
 class BocTemProvider: BasePlugin() {

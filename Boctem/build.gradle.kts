f404f0031dbd5b744bab6cea5a275f4152a61746
dah215 <duongah514hp@gmail.com>
2026-02-09 19:57:30 +0700

    Update build.gradle.kts

--- Boctem/build.gradle.kts
+++ Boctem/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         minSdk = 21
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -20,11 +20,11 @@ android {
 
 dependencies {
     val cloudstream by configurations
-    
-    // Dùng implementation để đảm bảo thư viện có mặt khi biên dịch
+    // Sử dụng bản master-SNAPSHOT cho plugin system
     cloudstream("com.github.recloudstream:cloudstream:master-SNAPSHOT")
-    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
     
+    // Ép kiểu implementation để trình biên dịch thấy class Requests và OkHttp
+    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jsoup:jsoup:1.17.2")
 }

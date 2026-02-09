1cbb23656fc3421a5b8cc4072f1236e782878c80
dah215 <duongah514hp@gmail.com>
2026-02-09 19:28:59 +0700

    Update build.gradle.kts

--- Boctem/build.gradle.kts
+++ Boctem/build.gradle.kts
@@ -11,12 +11,29 @@ android {
     defaultConfig {
         minSdk = 21
     }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
     val cloudstream by configurations
+    
+    // Dùng cloudstream cho plugin system
     cloudstream("com.github.recloudstream:cloudstream:pre-release")
     
-    // THÊM LẠI DÒNG NÀY ĐỂ FIX LỖI BUILD
+    // Dùng compileOnly để fix lỗi "Cannot access class Requests" khi Build
+    compileOnly("com.github.recloudstream:cloudstream:pre-release")
+    
+    // Thêm OkHttp để hỗ trợ Requests
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    
+    // Jsoup cho phân tích HTML
     implementation("org.jsoup:jsoup:1.17.2")
 }

48a2c8ddeabff298ad74a46ea741ab6cb31fd63e
dah215 <duongah514hp@gmail.com>
2026-02-09 21:15:59 +0700

    Update build.gradle.kts

--- Boctem/build.gradle.kts
+++ Boctem/build.gradle.kts
@@ -1,17 +1,29 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "com.boctem"
+
+    // Các giá trị này có thể bỏ nếu muốn kế thừa từ root,
+    // giữ lại để tránh lỗi khi AGP thay đổi hành vi
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
-    val cloudstream by configurations
     val csVersion = "master-SNAPSHOT"
 
-    // Đóng gói plugin
-    cloudstream("com.github.recloudstream:cloudstream:$csVersion")
-    
-    // Hỗ trợ biên dịch
     compileOnly("com.github.recloudstream:cloudstream:$csVersion")
-    
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jsoup:jsoup:1.17.2")
 }

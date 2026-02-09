005e6dc15fe34999c6dc8658d9e709767f3df064
dah215 <duongah514hp@gmail.com>
2026-02-09 21:20:01 +0700

    Update build.gradle.kts

--- Boctem/build.gradle.kts
+++ Boctem/build.gradle.kts
@@ -1,28 +1,35 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    // Nếu bạn cần plugin cloudstream cho module này, giữ dòng dưới;
+    // nếu plugin không có sẵn trong pluginManagement, build sẽ báo lỗi.
+    // id("com.lagradost.cloudstream3.gradle")
 }
 
 android {
     namespace = "com.boctem"
-
-    // Các giá trị này có thể bỏ nếu muốn kế thừa từ root,
-    // giữ lại để tránh lỗi khi AGP thay đổi hành vi
     compileSdk = 34
 
     defaultConfig {
         minSdk = 21
+        targetSdk = 34
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs += "-Xskip-metadata-version-check"
+    }
 }
 
 dependencies {
     val csVersion = "master-SNAPSHOT"
 
+    // nếu cloudstream artifact cần ở dạng compileOnly / implementation
     compileOnly("com.github.recloudstream:cloudstream:$csVersion")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jsoup:jsoup:1.17.2")

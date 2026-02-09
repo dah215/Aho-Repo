e2b5bc642ac06beaa63c7a946110ec1ce3939ad1
dah215 <duongah514hp@gmail.com>
2026-02-09 20:31:24 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile // THÊM DÒNG NÀY
-
 buildscript {
     repositories {
         google()
@@ -7,9 +5,9 @@ buildscript {
         maven { url = uri("https://jitpack.io") }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.2.2") // Nâng cấp AGP
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0") // Nâng cấp lên Kotlin 2.1.0
     }
 }
 
@@ -25,10 +23,12 @@ subprojects {
         }
     }
 
-    // THÊM ĐOẠN NÀY ĐỂ ÉP JVM 1.8 CHO KOTLIN
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = "1.8"
+    // Cấu hình ép kiểu JVM 1.8 và bỏ qua lỗi Metadata cho Kotlin 2.x
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            // Thêm cờ này để ép trình biên dịch đọc các class có metadata mới hơn
+            freeCompilerArgs.add("-Xskip-metadata-version-check")
         }
     }
 }

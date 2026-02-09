5a8171c6906a5696e574cd9ae21c3814312e60bb
dah215 <duongah514hp@gmail.com>
2026-02-09 21:24:26 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,21 +2,18 @@ rootProject.name = "CloudstreamBocTem"
 
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
     }
 }
 
 dependencyResolutionManagement {
-    // Chuyển sang PREFER_SETTINGS để đảm bảo JitPack được ưu tiên
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
     }
 }
-
-include(":boctem")

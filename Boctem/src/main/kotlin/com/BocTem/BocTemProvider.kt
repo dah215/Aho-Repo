6d747d95e85f7105400898a4cfa102ddb443dd4c
dah215 <duongah514hp@gmail.com>
2026-02-09 18:28:01 +0700

    Update BocTemProvider.kt

--- Boctem/src/main/kotlin/com/BocTem/BocTemProvider.kt
+++ Boctem/src/main/kotlin/com/BocTem/BocTemProvider.kt
@@ -1,12 +1,13 @@
 package com.BocTem
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.APIHolder
 import android.content.Context
 
 @CloudstreamPlugin
-class BocTemProvider : Plugin() {
-    override fun load(context: Context) {
-        registerMainAPI(BocTem())
+class BocTemProvider {
+    fun load(context: Context) {
+        // Directly add to API holder
+        APIHolder.allProviders.add(BocTem())
     }
 }

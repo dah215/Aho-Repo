18897f7dfb56d4d8d2e948bc9fb342f7aed57209
dah215 <duongah514hp@gmail.com>
2026-02-09 19:48:10 +0700

    Update build.gradle.kts

--- Boctem/build.gradle.kts
+++ Boctem/build.gradle.kts
@@ -16,22 +16,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
-    // Đã xóa kotlinOptions để tránh lỗi version
 }
 
 dependencies {
     val cloudstream by configurations
     
-    // SỬA: Đổi "pre-release" thành "master-SNAPSHOT"
-    cloudstream("com.github.recloudstream:cloudstream:pre-release-SNAPSHOT")
+    // Dùng implementation để đảm bảo thư viện có mặt khi biên dịch
+    cloudstream("com.github.recloudstream:cloudstream:master-SNAPSHOT")
+    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
     
-    // SỬA: Đổi "pre-release" thành "master-SNAPSHOT"
-    compileOnly("com.github.recloudstream:cloudstream:pre-release-SNAPSHOT")
-    
-    // Thêm OkHttp để hỗ trợ Requests
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    
-    // Jsoup cho phân tích HTML
     implementation("org.jsoup:jsoup:1.17.2")
 }

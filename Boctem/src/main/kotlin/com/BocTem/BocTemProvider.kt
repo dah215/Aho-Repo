dad4aa43dfbe4d82f0f5f3a5bd9fe868c6e837c1
dah215 <duongah514hp@gmail.com>
2026-02-09 19:16:26 +0700

    Update BocTemProvider.kt

--- Boctem/src/main/kotlin/com/BocTem/BocTemProvider.kt
+++ Boctem/src/main/kotlin/com/BocTem/BocTemProvider.kt
@@ -1,13 +1,14 @@
-package com.BocTem
+package com.boctem // Sửa thành chữ thường
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.APIHolder
+import com.lagradost.cloudstream3.plugins.Plugin // Thêm import này
 import android.content.Context
+import com.lagradost.cloudstream3.APIHolder
 
 @CloudstreamPlugin
-class BocTemProvider {
-    fun load(context: Context) {
-        // Directly add to API holder
+class BocTemProvider : Plugin() { // Kế thừa Plugin
+    override fun load(context: Context) {
+        // Tất cả các provider phải được đăng ký ở đây
         APIHolder.allProviders.add(BocTem())
     }
 }

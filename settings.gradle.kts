93b57ecbe7020dd0d9eb1388e4137cbade4e9df0
dah215 <duongah514hp@gmail.com>
2026-02-10 22:11:00 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,6 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://jitpack.io")
     }
-    resolutionStrategy {
-        eachPlugin {
-            // Khi gặp plugin Cloudstream, ta chỉ định rõ artifact thực tế trên JitPack
-            if (requested.id.id == "com.lagradost.cloudstream3.gradle") {
-                useModule("com.github.recloudstream:gradle:${requested.version}")
-            }
-        }
-    }
 }
 
 dependencyResolutionManagement {

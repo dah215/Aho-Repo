39f67cf92f7cefbe6c1ec341945e5e52bb348356
dah215 <duongah514hp@gmail.com>
2026-02-11 09:32:22 +0700

    Update build.gradle.kts

--- Boctem/build.gradle.kts
+++ Boctem/build.gradle.kts
@@ -1,13 +1,13 @@
 // use an integer for version numbers
-version = 5
+version = 1
 
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "(Mexican) Anime"
-    language    = "mx"
-    authors = listOf("Aho-Repo")
+    description = "Vietnamese anime streaming site"
+    language    = "vi"
+    authors = listOf("BocTem")
 
     /**
     * Status int as the following:
@@ -16,13 +16,13 @@ cloudstream {
     * 2: Slow
     * 3: Beta only
     * */
-    status = 1 // will be 3 if unspecified
+    status = 3 // will be 3 if unspecified
 
     // List of video source types. Users are able to filter for extensions in a given category.
     // You can find a list of available types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Movie","Anime","AnimeMovie")
-    iconUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBY6zyssWF7b3GT1v3Q1-_kwxYqmKm-YjzcyeuHldsHg&s"
+    tvTypes = listOf("Anime","AnimeMovie")
+    iconUrl = "https://boctem.com/favicon.ico"
 
     isCrossPlatform = false
 }

5ec96a1b752bf4c1905c87b751bd9bab50eb7252
dah215 <duongah514hp@gmail.com>
2026-03-04 07:41:10 +0700

    Update PhimMoiChill.kt

--- PhimMoiChill/src/main/kotlin/com/phimmoichill/PhimMoiChill.kt
+++ PhimMoiChill/src/main/kotlin/com/phimmoichill/PhimMoiChill.kt
@@ -14,7 +14,7 @@ class PhimMoiChillPlugin : Plugin() {
 }
 
 class PhimMoiChillProvider : MainAPI() {
-    override var mainUrl = "https://phimmoichill.my"
+    override var mainUrl = "https://phimmoichill.you"
     override var name = "PhimMoiChill"
     override val hasMainPage = true
     override var lang = "vi"
